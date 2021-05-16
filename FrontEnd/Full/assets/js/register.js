c90abf9efb07d86d2566ff4017d490f8d8bb21a2
Huynh Kim Phat <hkimphat2012@gmail.com>
2021-05-16 19:41:38 +0700

    Update Admin

---
+++ FrontEnd/Full/assets/js/register.js
@@ -0,0 +1,23 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+Validator({
+    form: "#form-register",
+    rules: [
+        // rule email
+        Validator.isRequired("#email"),
+        Validator.isEmail("#email"),
+        // rule password
+        Validator.isRequired("#password"),
+        Validator.password("#password"),
+        // rule confirm password
+        Validator.isRequired("#confirm-password"),
+        Validator.confirmPassword("#confirm-password", "#password"),
+    ],
+    submitSuccess: (data) => {
+        alert("No error in validation");
+        console.log(data);
+    },
+});
+
+console.log(isFirstSubmit);
