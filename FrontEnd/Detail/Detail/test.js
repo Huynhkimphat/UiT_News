ec94bb4101f95a8e5d0709919c298d8e8c5c9616
Huynh Kim Phat <hkimphat2012@gmail.com>
2021-05-16 21:08:06 +0700

    Udpate Detail Page

--- FrontEnd/Detail/Detail/test.js
+++ FrontEnd/Detail/Detail/test.js
@@ -1,18 +1,20 @@
-
+$(document).ready(function() {
     $(function() {
         var saveComment = function(data) {
-
             // Convert pings to human readable format
             $(Object.keys(data.pings)).each(function(index, userId) {
                 var fullname = data.pings[userId];
-                var pingText = '@' + fullname;
-                data.content = data.content.replace(new RegExp('@' + userId, 'g'), pingText);
+                var pingText = "@" + fullname;
+                data.content = data.content.replace(
+                    new RegExp("@" + userId, "g"),
+                    pingText
+                );
             });
 
             return data;
-        }
-        $('#comments-container').comments({
-            profilePictureURL: 'https://scontent.fpnh22-3.fna.fbcdn.net/v/t1.6435-1/p240x240/149943665_467969470998408_3401684646876157977_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=7206a8&_nc_ohc=cGiUDb5rVQ0AX_5aRfx&_nc_ht=scontent.fpnh22-3.fna&tp=6&oh=6f710e994a6a95fe7038809a79d515f6&oe=60C80421',
+        };
+        $("#comments-container").comments({
+            profilePictureURL: "https://scontent.fpnh22-3.fna.fbcdn.net/v/t1.6435-1/p240x240/149943665_467969470998408_3401684646876157977_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=7206a8&_nc_ohc=cGiUDb5rVQ0AX_5aRfx&_nc_ht=scontent.fpnh22-3.fna&tp=6&oh=6f710e994a6a95fe7038809a79d515f6&oe=60C80421",
             currentUserId: 1,
             roundProfilePictures: true,
             textareaRows: 1,
@@ -22,11 +24,16 @@
             scrollContainer: $(window),
             searchUsers: function(term, success, error) {
                 setTimeout(function() {
-                    success(usersArray.filter(function(user) {
-                        var containsSearchTerm = user.fullname.toLowerCase().indexOf(term.toLowerCase()) != -1;
-                        var isNotSelf = user.id != 1;
-                        return containsSearchTerm && isNotSelf;
-                    }));
+                    success(
+                        usersArray.filter(function(user) {
+                            var containsSearchTerm =
+                                user.fullname
+                                .toLowerCase()
+                                .indexOf(term.toLowerCase()) != -1;
+                            var isNotSelf = user.id != 1;
+                            return containsSearchTerm && isNotSelf;
+                        })
+                    );
                 }, 500);
             },
             getComments: function(success, error) {
@@ -61,4 +68,4 @@
             },
         });
     });
-
+});
